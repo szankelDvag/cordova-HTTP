465bb67b8320edc65cb0e0d9e855bf0671eb33e0
Aaron Lee <aaron@switch168.com>
2016-04-14 12:14:56 -0700

    Android fix

--- src/android/com/synconset/CordovaHTTP/CordovaHttpPlugin.java
+++ src/android/com/synconset/CordovaHTTP/CordovaHttpPlugin.java
@@ -75,14 +75,9 @@ public class CordovaHttpPlugin extends CordovaPlugin {
             String urlString = args.getString(0);
             JSONObject jsonObj = args.getJSONObject(1);
             JSONObject headers = args.getJSONObject(2);
-            HashMap<String, String> headersMap = this.addToMap(this.globalHeaders, headers);
+            HashMap<String, String> headersMap = this.getStringMapFromJSONObject(headers);
             CordovaHttpPostJson postJson = new CordovaHttpPostJson(urlString, jsonObj, headersMap, callbackContext);
             cordova.getThreadPool().execute(postJson);
-        } else if (action.equals("useBasicAuth")) {
-            String username = args.getString(0);
-            String password = args.getString(1);
-            this.useBasicAuth(username, password);
-            callbackContext.success();
         } else if (action.equals("enableSSLPinning")) {
             try {
                 boolean enable = args.getBoolean(0);
