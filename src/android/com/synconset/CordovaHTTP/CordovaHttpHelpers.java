46a39011028578d653fb1f4ba7ca61a5232bb904
Sebastian Zankel <sebastian.zankel@dvag.com>
2017-05-19 16:11:09 +0200

    Added method for application/x-www-form-urlencoded content type

--- src/android/com/synconset/CordovaHTTP/CordovaHttpHelpers.java
+++ src/android/com/synconset/CordovaHTTP/CordovaHttpHelpers.java
@@ -5,20 +5,17 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.LinkedHashMap;
+import java.io.UnsupportedEncodingException;
 
 public class CordovaHttpHelpers {
 
-    public static Map<String, List<String>> splitQuery(URL url) throws UnsupportedEncodingException {
-        final Map<String, List<String>> query_pairs = new LinkedHashMap<String, List<String>>();
-        final String[] pairs = url.getQuery().split("&");
+    public static Map<String, String> splitQuery(URL url) throws UnsupportedEncodingException {
+        Map<String, String> query_pairs = new LinkedHashMap<String, String>();
+        String query = url.getQuery();
+        String[] pairs = query.split("&");
         for (String pair : pairs) {
-            final int idx = pair.indexOf("=");
-            final String key = idx > 0 ? URLDecoder.decode(pair.substring(0, idx), "UTF-8") : pair;
-            if (!query_pairs.containsKey(key)) {
-                query_pairs.put(key, new LinkedList<String>());
-            }
-            final String value = idx > 0 && pair.length() > idx + 1 ? URLDecoder.decode(pair.substring(idx + 1), "UTF-8") : null;
-            query_pairs.get(key).add(value);
+            int idx = pair.indexOf("=");
+            query_pairs.put(URLDecoder.decode(pair.substring(0, idx), "UTF-8"), URLDecoder.decode(pair.substring(idx + 1), "UTF-8"));
         }
         return query_pairs;
     }
